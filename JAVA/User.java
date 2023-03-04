b58b5b8cbb0a5e770227d014bed256911b47cc77
sophia castiglione <sophiacastiglione@sophias-mac.wireless.rit.edu>
2023-03-04 17:23:35 -0500

    addUser method created

--- JAVA/User.java
+++ JAVA/User.java
@@ -1,24 +1,55 @@
-public class User {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-    private String userID;
+public class User {
+    private Connection conn; // not working yet
+    private String username;
     private String password;
     private String country;
     /**
      * Makes a User
-     * @param userID: String
+     * @param username: String
      * @param password: String
      */
-    public User(String userID, String password, String country){
-        this.userID = userID;
+    public User(String username, String password, String country){
+        this.username = username;
         this.password = password;
         this.country = country;
     }
 
+    public int addUser(User user) {
+        try {
+            int result = 0;
+            String sql = "INSERT INTO userInfo (username, password, country) VALUES (?, ?, ?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getPassword());
+            ps.setString(3, user.getCountry());
+
+            result = ps.executeUpdate();
+            return(result);
+        } // end of try
+
+        catch(SQLException sqle) {
+            sqle.printStackTrace();
+            return -1;
+        } // end of catch
+    }
+
+
+
     /**
      * @return String of User's ID
      */
-    public String getUserID(){
-        return this.userID;
+    public String getUsername(){
+        return this.username;
+    }
+    /**
+     * @return String of User's password
+     */
+    public String getPassword(){
+        return this.password;
     }
     /**
      * @return String of user's country
